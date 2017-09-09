4cd0dcdb71b9484b2d246bafd06c804ffe174482
Randy Nitahara <randyn@Randys-MacBook-Pro.local>
2017-09-08 20:09:01 -1000

    declared two variables

--- exercises.js
+++ exercises.js
@@ -26,7 +26,8 @@ console.log(testGreeting); // printing the output value of the function.
  *
  * These two variables will be used to invoke the functions #2 - #5.
 */ 
-
+var bango1 = 7;
+var bango2 = 11;
 
 
 /*
